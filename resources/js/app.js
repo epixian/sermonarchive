0975b2a794abc632d35bdd3f2afebd8a18ef5b3c
epixian <epixian@epixian.com>
2020-05-11 23:07:33 -0400

    remove vue, add alpinejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+// window.axios = require('axios');
+
+// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+import 'alpinejs';
+
+// window.Vue = require('vue');
+
+// const app = new Vue({
+//     el: '#app',
+// });
