988dfe5a967d02c799dc7f148da436456e41c285
epixian <epixian@epixian.com>
2020-05-11 23:06:15 -0400

    Create SermonFactory.php

---
+++ database/factories/SermonFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Sermon;
+use Carbon\Carbon;
+use Faker\Generator as Faker;
+
+$factory->define(Sermon::class, function (Faker $faker) {
+    return [
+        'name' => $faker->sentence,
+        'publish_date' => Carbon::now(),
+    ];
+});
