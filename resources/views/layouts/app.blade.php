9b6fa65c62133c1b749273f73f018c2992079239
epixian <epixian@epixian.com>
2020-05-12 00:09:53 -0400

    add content section padding

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -126,7 +126,9 @@
             </header>
             <main>
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    @yield('content')
+                    <div class="px-4 py-8 sm:px-0">
+                        @yield('content')
+                    </div>
                 </div>
             </main>
         </div>
