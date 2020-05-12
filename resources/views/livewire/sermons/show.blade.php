93c329e1046b6a2c34f553b7261be16092f14fb0
epixian <epixian@epixian.com>
2020-05-12 00:39:48 -0400

    remove js construction, make video full-width on mobile

--- resources/views/livewire/sermons/show.blade.php
+++ resources/views/livewire/sermons/show.blade.php
@@ -1,24 +1,11 @@
-<div>
-    {{ $sermon }}
-
+<div class="-mx-4 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
     <div class="w-full pb-2/3 relative">
-        <video-js id="my-video" class="video-js">
-
+        <video-js id="my-video" class="video-js" controls autoplay preload="none" data-setup='{ "liveui": true }'>
+            <source src="https://stream.newlifeglenside.com/hls/{{ $sermon->stream_key }}.m3u8" type="application/vnd.apple.mpegurl">
+            <p class="vjs-no-js">To watch this video, please enable Javascript or use a browser that supports HTML5 video.</p>
         </video-js>
     </div>
 
     <link href="https://vjs.zencdn.net/7.7.6/video-js.css" rel="stylesheet" />
     <script src="https://vjs.zencdn.net/7.7.6/video.js"></script>
-    <script>
-        var player = new videojs('my-video', {
-            liveui: true,
-            controls: true,
-            autoplay: true,
-            preload: 'none',
-        });
-        player.src({
-            src: 'https://stream.newlifeglenside.com/hls/{{ $sermon->stream_key }}.m3u8',
-            type: 'application/vnd.apple.mpegurl',
-        });
-    </script>
 </div>
