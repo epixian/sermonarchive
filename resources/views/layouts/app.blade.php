3cc6b77f0edf756593d505828d20fb6c6961df57
epixian <epixian@epixian.com>
2020-05-12 13:51:16 -0400

    initialize as hidden to avoid rendering before alpine loads

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,7 +89,7 @@
             </div>
 
             <!-- Mobile menu -->
-            <div x-show="menuOpen" @click.away="menuOpen = false" class="sm:hidden" :class="{ 'block': menuOpen, 'hidden': !menuOpen }">
+            <div x-show="menuOpen" @click.away="menuOpen = false" class="hidden sm:hidden" :class="{ 'block': menuOpen, 'hidden': !menuOpen }">
                 <div class="pt-2 pb-3 space-y-1">
                     <a href="/admin" class="block pl-3 pr-4 py-2 border-l-4 text-base font-medium transition duration-150 ease-in-out {{ Request::is('admin') ? 'border-indigo-500 text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700' : 'border-transparent text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300' }}">Dashboard</a>
                     <a href="/admin/sermons" class="block pl-3 pr-4 py-2 border-l-4 text-base font-medium transition duration-150 ease-in-out {{ Request::is('admin/sermons') ? 'border-indigo-500 text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700' : 'border-transparent text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300' }}">Sermons</a>
