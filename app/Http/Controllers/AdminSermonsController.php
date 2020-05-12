0cdef183b9640e4de405b0ce89700c932dd82c56
epixian <epixian@epixian.com>
2020-05-11 23:08:15 -0400

    Create AdminSermonsController.php

---
+++ app/Http/Controllers/AdminSermonsController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Sermon;
+use Illuminate\Http\Request;
+
+class AdminSermonsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // authorize user?
+        $user = auth()->user();
+
+        // fetch data
+        $sermons = $user->sermons;
+
+        // return view
+        return view('admin.sermons.index', compact('sermons'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // authorize user?
+        $user = auth()->user();
+
+        // validate request
+        $validated = $request->validate([
+            'name' => 'required',
+            'publish_date' => 'required',
+        ]);
+
+        // persist data
+        $sermon = $user->sermons()->create($validated);
+
+        // return view
+        return view('admin.sermons.show', compact('sermon'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Sermon  $sermon
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Sermon $sermon)
+    {
+        return view('admin.sermons.show', compact('sermon'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Sermon  $sermon
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Sermon $sermon)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Sermon  $sermon
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Sermon $sermon)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Sermon  $sermon
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Sermon $sermon)
+    {
+        //
+    }
+}
