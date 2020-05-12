89ad10fb1874bf01691001fb21ce548bbcea59a2
epixian <epixian@epixian.com>
2020-05-11 23:12:21 -0400

    admin, admin sermon routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::middleware('auth')->prefix('/admin')->group(function () {
+    Route::get('/', 'AdminController@index')->name('admin');
+
+    Route::get('/sermons', 'AdminSermonsController@index');
+    Route::post('/sermons', 'AdminSermonsController@store');
+    Route::get('/sermons/{sermon}', 'AdminSermonsController@show');
+});
