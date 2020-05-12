2d83eeb7c5a1837108c4b36fdf1209445378b202
epixian <epixian@epixian.com>
2020-05-12 15:56:01 -0400

    Create SermonsController.php

---
+++ app/Http/Controllers/SermonsController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Sermon;
+use Illuminate\Http\Request;
+
+class SermonsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $sermons = Sermon::all();
+        return view('sermons.show', compact('sermons'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Sermon  $sermon
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Sermon $sermon)
+    {
+        return view('sermons.show', compact('sermon'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Sermon  $sermon
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Sermon $sermon)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Sermon  $sermon
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Sermon $sermon)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Sermon  $sermon
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Sermon $sermon)
+    {
+        //
+    }
+}
