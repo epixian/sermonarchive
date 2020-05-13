98078f37309c6f4f37ff96c59d72efa8f672e40f
epixian <epixian@epixian.com>
2020-05-13 15:10:27 -0400

    remove services and sermons relationship, add gravatar url

--- app/User.php
+++ app/User.php
@@ -5,9 +5,11 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
+    use HasRoles;
     use Notifiable;
 
     /**
@@ -16,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'breeze_id',
     ];
 
     /**
@@ -38,20 +40,27 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the sermons belonging to this user.
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     * Get either a Gravatar URL or complete image tag for a specified email address.
+     *
+     * @param string $email The email address
+     * @param string $s Size in pixels, defaults to 80px [ 1 - 2048 ]
+     * @param string $d Default imageset to use [ 404 | mp | identicon | monsterid | wavatar ]
+     * @param string $r Maximum rating (inclusive) [ g | pg | r | x ]
+     * @param boolean $img True to return a complete IMG tag False for just the URL
+     * @param array $attributes Optional, additional key/value attributes to include in the IMG tag
+     * @return String containing either just a URL or a complete image tag
+     * @source https://gravatar.com/site/implement/images/php/
      */
-    public function sermons()
-    {
-        return $this->hasMany(Sermon::class);
-    }
-
-    /**
-     * Get the services belonging to this user.
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
-    public function services()
-    {
-        return $this->hasMany(Service::class);
+    function gravatar_url($s = 80, $d = 'mp', $r = 'g', $img = false, $attributes = array() ) {
+        $url = 'https://www.gravatar.com/avatar/';
+        $url .= md5(strtolower(trim($this->email)));
+        $url .= "?s=$s&d=$d&r=$r";
+        if ($img) {
+            $url = '<img src="' . $url . '"';
+            foreach ($attributes as $key => $val)
+                $url .= ' ' . $key . '="' . $val . '"';
+            $url .= ' />';
+        }
+        return $url;
     }
 }
