c4e4ac3e8c836821b2bc57a66645225cfa07d841
epixian <epixian@epixian.com>
2020-05-13 15:15:27 -0400

    add spatie/laravel-permission

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,22 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+
+        // Reset cached roles/permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        Permission::create(['name' => 'edit_users']);
+        Permission::create(['name' => 'edit_services']);
+        Permission::create(['name' => 'edit_sermons']);
+        Permission::create(['name' => 'participate']);
+
+        $role = Role::create(['name' => 'regular_user'])
+            ->givePermissionTo(['participate']);
+        $role = Role::create(['name' => 'stream_technician'])
+            ->givePermissionTo(['edit_sermons', 'participate']);
+        $role = Role::create(['name' => 'admin'])
+            ->givePermissionTo(Permission::all());
+        $role = Role::create(['name' => 'owner'])
+            ->givePermissionTo(Permission::all());
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,30 @@ Route::get('/', function () {
     return redirect('/sermons');
 });
 
+Route::get('/test', function() {
+    $breeze = new Breeze(env('BREEZE_API_KEY'));
+
+    // get person by email
+    // $email = 'oelschlegel@gmail.com';
+    // $people = json_decode($breeze->url('https://newlifeglenside.breezechms.com/api/people?details=1&filter_json={"1786141247":"' . $email . '"}'));
+    // foreach ($response as $person) {
+    //     if ($person->details->details->email_primary === $email) {
+    //         dump($person);
+    //     }
+    // }
+
+    // profile fields
+    // $profileFields = json_decode($breeze->url('https://newlifeglenside.breezechms.com/api/profile'));
+    // dump($profileFields);
+
+    // get person details by id
+    $id = '19739224';
+    $person = json_decode($breeze->url('https://newlifeglenside.breezechms.com/api/people/' . $id . '?details=1'));
+    foreach ($person->family as $member) {
+        dump($member->details);
+    }
+});
+
 Auth::routes(['verify' => true]);
 
 Route::get('/sermons', 'SermonsController@index');
