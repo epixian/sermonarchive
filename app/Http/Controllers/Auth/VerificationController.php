829635d1493b4902f8832611810828d97d37a4fb
epixian <epixian@epixian.com>
2020-05-13 15:09:26 -0400

    add verified actions

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Breeze;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\VerifiesEmails;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class VerificationController extends Controller
 {
@@ -38,5 +41,37 @@ class VerificationController extends Controller
         $this->middleware('auth');
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
+    }
+
+    /**
+     * The user has been verified.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    protected function verified(Request $request)
+    {
+        // link to breeze
+        $breeze = new Breeze(env('BREEZE_API_KEY'));
+        $email = auth()->user()->email;
+        $response = $breeze->url('https://newlifeglenside.breezechms.com/api/people?details=1&filter_json={"1786141247":"' . $email . '"}');
+        $found = false;
+        foreach (json_decode($response) as $person) {
+            if ($person->details->details->email_primary === $email) {
+                $found = true;
+                auth()->user()->update(['breeze_id' => $person->id]);
+                auth()->user()->fresh();
+                break;
+            }
+        }
+
+        if ($found) {
+            Session::flash('message', 'Success! Your account has been verified and linked.');
+        }
+        else {
+            Session::flash('message', 'Thank you for verifying your email address!  However, we did not find it in our system.  If you feel this is in error, please contact the church receptionist.');
+
+            // email Jan
+        }
     }
 }
