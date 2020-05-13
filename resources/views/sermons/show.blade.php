3332c36347fc2e0a15d010d445c957d4f6542a0d
epixian <epixian@epixian.com>
2020-05-13 18:44:59 -0400

    remove livewire

--- resources/views/sermons/show.blade.php
+++ resources/views/sermons/show.blade.php
@@ -2,6 +2,36 @@
 
 @section('title', $sermon->name)
 
+@section('actions')
+<div class="flex items-center space-x-2">
+
+    @can('edit_sermons')
+    <span class="inline-flex rounded-md shadow-sm">
+      <a href="{{ $sermon->path() }}/edit" class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
+        Edit Sermon
+      </a>
+    </span>
+    @endcan
+
+</div>
+@endsection
+
 @section('content')
-    <livewire:sermons.show :sermon="$sermon">
+<div class="-mx-4 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+    <video-js id="my-video" class="video-js vjs-big-play-centered h-full" controls autoplay preload="none" data-setup='{ "liveui": true }'>
+        <source src="https://stream.newlifeglenside.com/recordings/{{ $sermon->stream_key }}.flv" type="video/flv">
+        <source src="https://stream.newlifeglenside.com/hls/{{ $sermon->stream_key }}.m3u8" type="application/vnd.apple.mpegurl">
+        <p class="vjs-no-js">To watch this video, please enable Javascript or use a browser that supports HTML5 video.</p>
+    </video-js>
+</div>
+
+<div class="sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+    <h2 class="text-xl font-bold leading-tight text-gray-900">{{ $sermon->name }}</h2>
+    <div class="text-base text-gray-900">
+        {{ $sermon->description }}
+    </div>
+</div>
+
+<link href="https://vjs.zencdn.net/7.7.6/video-js.min.css" rel="stylesheet" />
+<script src="https://vjs.zencdn.net/7.7.6/video.min.js"></script>
 @endsection
