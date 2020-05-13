5438cb4c1ecc79f8f8c98664f6341e4cd0ed4e15
epixian <epixian@epixian.com>
2020-05-13 15:10:45 -0400

    Create ServiceFactory.php

---
+++ database/factories/ServiceFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Service;
+use Carbon\Carbon;
+use Faker\Generator as Faker;
+
+$factory->define(Service::class, function (Faker $faker) {
+    return [
+        'name' => 'Morning Worship Services',
+        'description' => '',
+        'service_date' => Carbon::now(),
+        'breeze_id' => '',
+    ];
+});
