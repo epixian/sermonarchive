a43811b210f39d645001b303fa8b32b4f926435f
epixian <epixian@epixian.com>
2020-05-13 22:54:06 -0400

    update table headers on sermon listing

--- resources/views/sermons/index.blade.php
+++ resources/views/sermons/index.blade.php
@@ -10,13 +10,13 @@
                 <thead>
                     <tr>
                         <th class="px-4 sm:px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                            Sermon Name
+                            Sermon Title
                         </th>
                         <th class="px-4 sm:px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                            Publish Date
+                            Date
                         </th>
                         <th class="px-4 sm:px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                            Other
+                            Speaker
                         </th>
                         @can('edit_sermon')
                         <th class="px-4 sm:px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
