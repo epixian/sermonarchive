00eec6584cc18eccd5eb73ff08d99f650a5cab05
epixian <epixian@epixian.com>
2020-05-15 13:32:57 -0400

    add old values for editing

--- resources/views/services/edit.blade.php
+++ resources/views/services/edit.blade.php
@@ -5,8 +5,13 @@
 @endsection
 
 @section('content')
-<form method="POST" action="/admin/services">
+<form method="POST" action="{{ $service ? $service->path() : '/admin/services' }}">
   @csrf
+  @if ($service)
+    @method('PUT')
+  @else
+    @method('POST')
+  @endif
   <div>
     <div>
       <div class="mt-6 sm:mt-5">
@@ -16,7 +21,7 @@
           </label>
           <div class="mt-1 sm:mt-0 sm:col-span-2">
             <div class="max-w-lg flex rounded-md shadow-sm">
-              <input id="name" name="name" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+              <input name="name" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" value="{{ old('name', $service->name) }}" />
             </div>
             <p class="mt-2 text-sm text-gray-500">Change as needed.  Default: "Morning Worship Services"</p>
           </div>
@@ -28,7 +33,7 @@
           </label>
           <div class="mt-1 sm:mt-0 sm:col-span-2">
             <div class="max-w-lg flex rounded-md shadow-sm">
-              <textarea id="description" name="description" rows="3" class="form-textarea block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"></textarea>
+              <textarea name="description" rows="3" class="form-textarea block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5">{{ old('description', $service->description) }}</textarea>
             </div>
             <p class="mt-2 text-sm text-gray-500">(Optional) Write any additional information useful to site visitors.</p>
           </div>
@@ -40,7 +45,7 @@
           </label>
           <div class="mt-1 sm:mt-0 sm:col-span-2">
             <div class="max-w-lg flex rounded-md shadow-sm">
-              <input id="publish_date" name="service_date" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+              <input name="service_date" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" value="{{ old('service_date', $service->service_date) }}" />
             </div>
             <p class="mt-2 text-sm text-gray-500">The date of the service in <em>YYYY-MM-DD</em>.</p>
           </div>
@@ -51,9 +56,9 @@
   <div class="mt-8 border-t border-gray-200 pt-5">
     <div class="flex justify-end">
       <span class="inline-flex rounded-md shadow-sm">
-        <button type="button" class="py-2 px-4 border border-gray-300 rounded-md text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
+        <a href="{{ $service->path() }}" class="py-2 px-4 border border-gray-300 rounded-md text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
           Cancel
-        </button>
+        </a>
       </span>
       <span class="ml-3 inline-flex rounded-md shadow-sm">
         <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
