501d37e4258b0ddb728132796b5f371edfb1c782
epixian <epixian@epixian.com>
2020-05-15 21:04:21 -0400

    remove all messaging scripts

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,1 @@
-// window.axios = require('axios');
-
-// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 import 'alpinejs';
-
-// window.Vue = require('vue');
-
-// const app = new Vue({
-//     el: '#app',
-// });
