adfd7db1dfa0e8beaecf6a78d2529da68444e095
epixian <epixian@epixian.com>
2020-05-15 21:08:46 -0400

    vue chat window components, socket.io setup

---
+++ resources/js/components/ChatMessages.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="border-t border-r border-l border-gray-300 bg-white shadow-sm">
+    <li v-for="message in messages" :key="message.id" class="flex text-sm text-gray-900 leading-5 px-2 py-1">
+      <div class="font-medium">
+          {{ message.user.name }}:
+      </div>
+      <div class="ml-1">
+        {{ message.message }}
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    props: ['messages'],
+  }
+</script>
+
+<style scoped>
+
+</style>
