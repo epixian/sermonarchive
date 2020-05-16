2c66cd681583e1411c77fec258e03bcf67a7ac6f
epixian <epixian@epixian.com>
2020-05-15 21:07:38 -0400

    add auth check route for broadcasting

--- routes/channels.php
+++ routes/channels.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::channel('App.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+// Broadcast::channel('App.User.{id}', function ($user, $id) {
+//     return (int) $user->id === (int) $id;
+// });
+
+Broadcast::channel('nlglive-channel', function ($user) {
+  return auth()->check() && auth()->user()->hasPermissionTo('participate');
 });
