3ea859a7fe76fe845d9f923d4ca82190262cf4f3
epixian <epixian@epixian.com>
2020-05-16 02:59:50 -0400

    Create RecordAttendance.vue

---
+++ resources/js/components/RecordAttendance.vue
@@ -0,0 +1,57 @@
+<template>
+  <transition
+    enter-active-class="transition ease-in duration-100"
+    enter-class="opacity-0"
+    enter-to-class="opacity-100"
+    leave-active-class="transition ease-out duration-75"
+    leave-class="opacity-100"
+    leave-to-class="opacity-0"
+    mode="out-in"
+    >
+    <div v-show="available.length" class="max-w-7xl mx-auto">
+      <div class="mt-4 mx-4 sm:mx-6 text-base text-center leading-6 font-medium">
+        Who is worshiping together with you today?
+      </div>
+      <div class="flex flex-wrap items-center justify-center mx-4 sm:mx-6 space-x-2">
+        <span v-for="person in available" :key="person.id" class="inline-flex rounded-md shadow-sm">
+          <button @click="select(person)" type="button" class="mt-2 inline-flex items-center px-4 py-2 border text-sm leading-5 font-medium rounded-md transition ease-in-out duration-150" :class="!selected.includes(person) ? 'border-gray-300 text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50' : 'border-indigo-300 text-indigo-700 bg-indigo-100 hover:bg-indigo-50 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo active:bg-indigo-200'">
+            {{ person.name }}
+          </button>
+        </span>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        available: [],
+        selected: [],
+      }
+    },
+    created() {
+      axios.get('/user/family')
+        .then(response => {
+          if (response.data.length === 1)
+            this.checkIn(response.data);
+          else
+            this.available = response.data;
+        });
+    },
+    methods: {
+      checkIn(ids) {
+        axios.post('/check-in', ids)
+          .then(console.log);
+      },
+      select(person) {
+        this.selected.push(person);
+        axios.post('/check-in', person)
+          .then(() => {
+            this.available = this.available.filter(item => item !== person);
+          });
+      }
+    }
+  }
+</script>
