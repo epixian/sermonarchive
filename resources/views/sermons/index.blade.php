69c11176c76a9661f7da2f63aec519b8119da8fb
epixian <epixian@epixian.com>
2020-05-19 17:33:45 -0400

    fix permission name and edit link

--- resources/views/sermons/index.blade.php
+++ resources/views/sermons/index.blade.php
@@ -18,7 +18,7 @@
                         <th class="px-4 sm:px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                             Speaker
                         </th>
-                        @can('edit_sermon')
+                        @can('edit_sermons')
                         <th class="px-4 sm:px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
                         @endcan
                     </tr>
@@ -35,9 +35,9 @@
                         <td class="px-4 sm:px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
                             {{ $sermon->speaker->full_name }}
                         </td>
-                        @can('edit_sermon')
+                        @can('edit_sermons')
                         <td class="px-4 sm:px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                            <a href="/admin/sermons/{{ $sermon->id }}" class="text-nl-blue-500 hover:text-nl-blue-800">Edit</a>
+                            <a href="/admin/sermons/{{ $sermon->id }}/edit" class="text-nl-blue-500 hover:text-nl-blue-800">Edit</a>
                         </td>
                         @endcan
                     </tr>
