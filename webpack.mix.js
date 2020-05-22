039700d3de25993de9e2c79f419fa40eb9ef6f03
epixian <epixian@epixian.com>
2020-05-22 15:55:59 -0400

    add moment, extract vendor libs

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,15 @@
 const mix = require('laravel-mix');
 const tailwind = require('laravel-mix-tailwind');
 
+/* For use with Moment.js */
+const webpack = require('webpack');
+mix.webpackConfig({
+  plugins: [
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+  ]
+});
+
 mix.js('resources/js/app.js', 'public/js')
   .sass('resources/sass/app.scss', 'public/css')
-  .tailwind('tailwind.config.js');
+  .tailwind('tailwind.config.js')
+  .extract();
