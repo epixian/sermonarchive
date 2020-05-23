dcaa9ce751fa83e3e85314bccda90ddd2b25f6c6
epixian <epixian@epixian.com>
2020-05-23 00:26:38 -0400

    use VideoPlayer

--- resources/views/sermons/show.blade.php
+++ resources/views/sermons/show.blade.php
@@ -16,11 +16,7 @@
 
 @section('content')
 <div class="-mx-4 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
-    <video-js id="my-video" class="video-js vjs-big-play-centered h-full" controls autoplay preload="none" data-setup='{ "liveui": true }'>
-        <source src="https://stream.newlifeglenside.com/recordings/{{ $sermon->stream_key }}.mp4" type="video/mp4">
-        <!-- <source src="https://stream.newlifeglenside.com/hls/{{ $sermon->stream_key }}.m3u8" type="application/vnd.apple.mpegurl"> -->
-        <p class="vjs-no-js">To watch this video, please enable Javascript or use a browser that supports HTML5 video.</p>
-    </video-js>
+    <video-player :sermon='{!! $sermon->toJson() !!}'></video-player>
 </div>
 
 <div class="sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
