b460d2acf277d893fba68388facd11fe4470d1cc
epixian <epixian@epixian.com>
2020-05-22 20:57:46 -0400

    closing tags on vue components

--- resources/views/services/current.blade.php
+++ resources/views/services/current.blade.php
@@ -19,7 +19,7 @@
 <div class="-mx-4 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8 lg:flex lg:items-start">
     <div id="videoplayer" class="lg:flex-1">
         @foreach ($service->sermons as $sermon)
-        <video-player :sermon='{!! $sermon->toJson() !!}' />
+        <video-player :sermon='{!! $sermon->toJson() !!}'></video-player>
         @endforeach
 
     </div>
