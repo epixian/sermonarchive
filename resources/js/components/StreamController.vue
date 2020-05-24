edeee1b42ac5ed51fd73889d403426ba9855fd90
epixian <epixian@epixian.com>
2020-05-24 08:05:41 -0400

    change button color

--- resources/js/components/StreamController.vue
+++ resources/js/components/StreamController.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-right">
     <span class="inline-flex rounded-md shadow-sm">
-      <button @click="goLive" type="button" class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150" :disabled="stream_started">
+      <button @click="goLive" type="button" class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-nl-blue-600 hover:bg-nl-blue-500 focus:outline-none focus:border-nl-blue-700 focus:shadow-outline-nl-blue active:bg-nl-blue-700 transition ease-in-out duration-150" :disabled="stream_started">
         Go Live
       </button>
     </span>
