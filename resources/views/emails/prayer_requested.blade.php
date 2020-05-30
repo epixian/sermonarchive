35b208011aae811c44061f2a5670177757906e27
epixian <epixian@epixian.com>
2020-05-30 15:07:23 -0400

    email blade templates

--- resources/views/emails/prayer_requested.blade.php
+++ resources/views/emails/prayer_requested.blade.php
@@ -1,5 +1,17 @@
-@component('mail::message')
-{{ $prayer['body'] }}
+<style>
+    @import url(http://fonts.googleapis.com/css?family=Pacifico);
+    /* Type styles for all clients */
+    * {
+     font-family: Helvetica, Arial, serif;
+    }
+    /* Type styles for WebKit clients */
+    @media screen and (-webkit-min-device-pixel-ratio:0) {
+      * {
+        font-family: Pacifico, Helvetica, Arial, serif !important;
+      }
+    }
+</style>
 
-Requested by {{ $from->name }} ({{ $from->email }}) on {{ Carbon\Carbon::now(env('TIMEZONE')) }} via live.newlifeglenside.com.
-@endcomponent
+<p>{{ $body }}</p>
+
+<small>Requested by {{ $sender['name'] }} ({{ $sender['email'] }}) on {{ Carbon\Carbon::now(env('TIMEZONE')) }} via live.newlifeglenside.com.</small>
---
+++ resources/views/emails/prayer_requested_plain.blade.php
@@ -0,0 +1,3 @@
+{{ $body }}
+
+Requested by {{ $sender['name'] }} ({{ $sender['email'] }}) on {{ Carbon\Carbon::now(env('TIMEZONE')) }} via live.newlifeglenside.com.
