cf7d40abf4f2fc8a8a3062f3e278ab310697d739
epixian <epixian@epixian.com>
2020-05-30 16:00:23 -0400

    inline font-family styles

--- resources/views/emails/prayer_requested.blade.php
+++ resources/views/emails/prayer_requested.blade.php
@@ -1,17 +1,5 @@
-<style>
-    @import url(http://fonts.googleapis.com/css?family=Pacifico);
-    /* Type styles for all clients */
-    * {
-     font-family: Helvetica, Arial, serif;
-    }
-    /* Type styles for WebKit clients */
-    @media screen and (-webkit-min-device-pixel-ratio:0) {
-      * {
-        font-family: Pacifico, Helvetica, Arial, serif !important;
-      }
-    }
-</style>
-
+<div style="font-family: Helvetica, Arial, serif;">
 <p>{{ $body }}</p>
 
 <small>Requested by {{ $sender['name'] }} ({{ $sender['email'] }}) on {{ Carbon\Carbon::now(env('TIMEZONE')) }} via live.newlifeglenside.com.</small>
+</div>
